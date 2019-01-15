488abe3a687caba6c1d3e52daa8c80b6445cfbc9
Tfrain <weichengdouble@outlook.com>
2019-01-15 13:35:37 +0800

    Site updated: 2019-01-15 13:35:37

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","d9f616cafaba8635cf1f7ad321991c39"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
